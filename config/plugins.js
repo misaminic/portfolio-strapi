9906aa3d608addc4511d8b5449aaec037ce6caa6
misaminic <misaminic@gmail.com>
2021-04-19 18:35:36 +0200

    make it better

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+// module.exports = ({ env }) => ({
+//   // ...
+//   upload: {
+//     provider: "cloudinary",
+//     providerOptions: {
+//       cloud_name: env("dbob4bstd"),
+//       api_key: env("663896672269774"),
+//       api_secret: env("7cSDvY7eGuWWRXneyJ9utmjl2Sk"),
+//     },
+//     actionOptions: {
+//       upload: {},
+//       delete: {},
+//     },
+//   },
+// });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2a03497b-6461-4195-bd9f-06eae584cce5'
+};
