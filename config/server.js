4f1c7527e24889891bb0049486f94b6c80816189
misaminic <misaminic@gmail.com>
2021-10-11 13:04:50 +0200

    first commit

--- config/server.js
+++ config/server.js
@@ -1,9 +1,30 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET', '6a642f70ecaa76d20c6de1899ecb34fd'),
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '6a642f70ecaa76d20c6de1899ecb34fd'),
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        host: env("DATABASE_HOST", "cluster0.ohdbi.mongodb.net"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "agatonsax"),
+        username: env("DATABASE_USERNAME", "development87"),
+        password: env("DATABASE_PASSWORD", "development87"),
+      },
+      options: {
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
+      },
     },
   },
 });
